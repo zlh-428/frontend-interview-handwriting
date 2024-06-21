d5a2e405910904f5333ad0eb2a7f80c82d78aca6
zlh-428 <zhoulihao@beisen.com>
2024-06-21 10:08:11 +0800

    fix: sleep function

---
+++ questions/Promise/sleep.mjs
@@ -0,0 +1,33 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+  const [num1, num2] = await Promise.all([promise1, promise2]);
+
+  return num1 + num2;
+};
+
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, millis)
+    })
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
