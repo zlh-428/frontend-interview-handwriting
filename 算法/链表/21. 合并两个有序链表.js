6051a4a4fb160d18f6638b03b758bdd9e8f46901
zlh-428 <zhoulihao@beisen.com>
2024-07-24 13:35:02 +0800

    feat: Merge listNode 21.

---
+++ 算法/链表/21. 合并两个有序链表.js
@@ -0,0 +1,45 @@
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  if (!list1 && !list2) return null;
+
+  if (!list1) return list2;
+
+  if (!list2) return list1;
+
+  let dummy = new ListNode(null);
+
+  let current = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+
+    current = current.next;
+  }
+
+  if (list1) {
+    current.next = list1;
+  }
+
+  if (list2) {
+    current.next = list2;
+  }
+
+  return dummy.next;
+}
